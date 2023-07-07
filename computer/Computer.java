1bc463e5298fd911b5d7b70ffce7806e2ad0e7d8
SAHIL THAKAR <30526170+sahilthakar10@users.noreply.github.com>
2023-07-07 19:51:15 +0530

    Create Computer.java

---
+++ computer/Computer.java
@@ -0,0 +1,19 @@
+class Computer {
+
+    private Ram ram;
+    private HardDisk hardDisk;
+
+    Computer(Ram ram, HardDisk hardDisk){
+        this.ram = ram;
+        this.hardDisk = hardDisk;
+    }
+
+    void getRam() {
+        ram.getRam();
+    }
+
+    void getHardDisk() {
+        hardDisk.getHardDisk();
+    }
+
+}
