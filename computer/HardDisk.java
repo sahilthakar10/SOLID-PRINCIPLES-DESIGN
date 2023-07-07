6a4c6a6f07f5b6d1a79c9293c961d83b4ffcc90a
SAHIL THAKAR <30526170+sahilthakar10@users.noreply.github.com>
2023-07-07 19:52:12 +0530

    solid design pattern - simple computer example

---
+++ computer/HardDisk.java
@@ -0,0 +1,9 @@
+package com.di.computer;
+
+class HardDisk {
+
+    void getHardDisk() {
+        System.out.println("My HardDisk");
+    }
+
+}
---
+++ computer/Ram.java
@@ -0,0 +1,9 @@
+package com.di.computer;
+
+class Ram {
+
+    void getRam() {
+        System.out.println("My Ram");
+    }
+
+}
